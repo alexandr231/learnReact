8268a03d0fb15a64205673d172bf6e55c3abe7c8
Alxeandro <79802233+alexandr231@users.noreply.github.com>
2023-01-04 17:24:38 +0300

    profile status local state

--- src/Components/ContentBar/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/ContentBar/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Preloader from '../../../Preloader/Preloader'
+import ProfileStatus from './ProfileStatus'
 
 function ProfileInfo(props) {
     return props.profile
         ? <div>
             <img src={props.profile.photos.large}></img>
             <div>{props.profile.fullName}</div>
-            <div>{props.profile.aboutMe}</div>
             <div>{props.profile.lookingForAJob
                 ? <div>
                     <div>Ищу работу</div>
                     <div>{props.profile.lookingForAJobDescription}</div>
                 </div>
                 : <div>Работаю</div>}
+            </div>
+            <div>
+                <ProfileStatus status = 'hahahahah'></ProfileStatus>
             </div>
         </div>
         : <Preloader />
---
+++ src/Components/ContentBar/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class ProfileStatus extends Component {
+    state = {
+        editMode: false
+    }
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.editMode
+                    ? <div>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}></input>
+                    </div>
+                    : <div>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                    </div>
+                }
+            </div>
+        )
+    }
+}
